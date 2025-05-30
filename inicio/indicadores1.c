3682edf19ed24331ab46af2281ef5673ae6a1ab9
Kigo524 <kigocode@gmail.com>
2025-05-30 09:30:42 -0600

    se agrega push como void

--- inicio/indicadores1.c
+++ inicio/indicadores1.c
@@ -36,13 +36,14 @@ bool isFull (Pila *pila){
 
 //push (pila, valor) agrega un valor en pila
 void push(Pila *pila, int valor) {
+    
     if (pila->tope == MAX - 1) {
         printf("La pila está llena.\n");
     } else {
         pila->tope++;
-        pila->arreglo[pila->tope] = arreglo;
-        printf("Elemento %d agregado a la pila.\n", valor);
+        pila->arreglo[pila->tope]=valor;
     }
+    printf("Elemento %d agregado a la pila.\n", valor);//se escribe afuera del else
 }
 
 int main() {
