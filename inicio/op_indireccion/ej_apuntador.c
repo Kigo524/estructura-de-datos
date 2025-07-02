5df3e7269c79be4b05e499540f9b153f9b989f80
Kigo524 <kigocode@gmail.com>
2025-07-01 22:56:20 -0600

    se crea un ejemplo de apuntador

--- inicio/op_indireccion/ej_apuntador.c
+++ inicio/op_indireccion/ej_apuntador.c
@@ -1,54 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    int n, m;
+    int num1, num2;
+
+    //declaro punteros
+    int *p1, *p2;
+
+    //Asignamos las direcciones de memoria de las varibales a los punteros
     
-    printf("ingrese un valor");
-    scanf("%d", &n); //ejemplo con 5
-    scanf("%d", &m); //ejemplo con 11
-    
-    printf("Imprime %d\n", n);
-    printf("Imprime %d\n", m);
-    int *p, *q, *r;
-    
-    p=&n;
-    q=&m;
-    
-    printf("Imprime %d\n", n);
-    printf("Imprime %d\n", m);
-
-    printf("P = %p\n", p);
-    printf("q = %d\n", &q);
-    printf("m = %d\n", &m);
-
-
-    *p=13;
-    *q=13;
-
-    printf("Imprime %d\n", n);
-    printf("Imprime %d\n", m);
-
-    printf("P = %p\n", p);
-    printf("q = %d\n", &q);
-    printf("m = %d\n", &m);
-
-    n=27;
-    *p= (n) + (*q);
-
-
-    printf("Imprime %d\n", n);
-    printf("Imprime %d\n", m);
-
-    printf("P = %p\n", p);
-    printf("q = %d\n", &q);
-    printf("m = %d\n", &m);
-
-    q=p;
-
-    printf("Despues de q=p\n");
-    printf("Imprime %d\n", n);
-    printf("Imprime %d\n", m);
-
-    printf("P = %p\n", p);
-    printf("q = %d\n", &q);
 }
